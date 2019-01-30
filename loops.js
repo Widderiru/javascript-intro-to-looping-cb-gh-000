93f69752af1db77ab1f61aab81b2874c6ea5eb77
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-30 11:05:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,18 @@ function whileLoop (number) {
   }
   return "done"
 }
+
+function doWhileLoop (array) {
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+  }
+
+  do {
+    console.log("doo-bee-doo-bee-doo");
+    incrementVariable();
+  } while (i < 5);
+  do {
+    array.pop()
+  } while (array.length >0 && incrementVariable())
+}
