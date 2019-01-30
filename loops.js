453035c92784e15b1b05bd475ddd5e928d27d0a3
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-30 10:31:20 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop (array) {
+  for (var i = 1 ; i < 26; i++) {
+    if (i == 1) {
+      array.push("I am 1 strange loop.");
+      return array
+    }
+    else {
+      array.push("I am ${i} strange loops.");
+      return array
+    }
+  }
+}
